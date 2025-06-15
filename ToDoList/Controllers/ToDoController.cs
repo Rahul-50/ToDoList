636ef1b0488d23f3b1b5d145a7dff3d478415675
Rahul-50 <rahulkadam9692@gmail.com>
2025-06-15 08:06:13 +0530

    Add project files.

---
+++ ToDoList/Controllers/ToDoController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using ToDoList.Data;
+using ToDoList.Models;
+
+public class TodoController : Controller
+{
+    private readonly TodoContext _context;
+
+    public TodoController(TodoContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.TodoItems.ToListAsync());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(TodoItem item)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(item);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var item = await _context.TodoItems.FindAsync(id);
+        return View(item);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(TodoItem item)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Update(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(item);
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var item = await _context.TodoItems.FindAsync(id);
+        return View(item);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var item = await _context.TodoItems.FindAsync(id);
+        _context.TodoItems.Remove(item);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using ToDoList.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ? Register only SQL Server provider for TodoContext
+builder.Services.AddDbContext<TodoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Todo}/{action=Index}/{id?}");
+
+app.Run();
